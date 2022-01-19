7684887495e448f7115aee0bfbb1197028b14145
Jeremy <asirwaadj@gmail.com>
2022-01-19 23:57:38 +0530

    Added pages

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './NavBar.css'
-
+import logo from './151517978_1629864483_IMG-20201010-WA0000.jpg'
 export const Navbar = () => {
     return (
         <div className='navbarcontainer'>
@@ -17,8 +17,10 @@ export const Navbar = () => {
             </details>
         </div>
         <div className="titlecon">
-        <i class="fas fa-arrow-up"></i>
-        <span className='title_nav'>Step Up</span>
+            <div className='logoandtitle'>
+                <img className='logoimg' src= { logo } alt="" />
+                <span className='title_nav'>Step Up</span>
+            </div>
         </div>
         </div>
     )
--- src/components/Section2/Section2.js
+++ src/components/Section2/Section2.js
@@ -4,6 +4,11 @@ import './section2.css'
 const Section2 = () => {
     return (
         <div className='section2'>
+            <div class="custom-shape-divider-top-1642607151">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+    </svg>
+</div>
         </div>
     );
 }
