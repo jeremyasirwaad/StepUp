87e52ead7a61be77fd2eed689fde72a1160885a8
Jeremy <asirwaadj@gmail.com>
2022-01-20 00:43:48 +0530

    landing page done

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,13 +7,15 @@ export const Landing = () => {
     return (
         <div className='Landingpage'>
             <div className="landingcontents">
-                
+                <p>Welcome to the <span className='colorland'>STEP UP</span> Program, a <span className='colorland'>gSARC</span> initiative to groom FullStack developers for startups</p>
+                <button class="button-56">Get Started</button>
+                <i class="fas fa-angle-double-down vert-move"></i>
             </div>
             <div class="custom-shape-divider-bottom-1642609008">
-    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
-    </svg>
-</div>
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+                </svg>
+            </div>
 
         </div>
     )
